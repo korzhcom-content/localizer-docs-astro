1e2301f4d397fe5dd96ab589ec9bef0240361f84
Oleksandr Melnychenko <green@crocodile.in.ua>
2025-03-21 03:54:34 +0200

    Update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,9 @@ export default defineConfig({
 	site,
 	base,
 	trailingSlash: "never",
+	build: {
+		assets: '_loc_astro'
+	},
 	integrations: [
 		starlight({
 			title: 'Localizer',
--- links.js
+++ links.js
@@ -7,7 +7,7 @@ export default [
     {
         id: 2,
         label: "Demo",
-        link: "https://demo.korzh.com/easyquery-asp-net-core-razor?_gl=1*7ld73a*_ga*MTY5NTk5MzA2OC4xNzMxMDE3OTkz*_ga_TTN7QK6N6X*MTczNzU2MjIyMS4xMS4xLjE3Mzc1NjMyNTAuMzUuMC4w",
+        link: "https://demo.korzh.com/easyquery-asp-net-core-razor",
     },
     {
         id: 3,
